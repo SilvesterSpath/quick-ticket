6517abf9978b604b295e36cf387b2f66cc1be2eb
SilvesterSpath <spath.sz@gmail.com>
2025-06-26 10:38:48 +0200

    ticketItem

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,8 +1,7 @@
 import { getTickets } from '@/actions/ticket.actions';
-import Link from 'next/link';
-import { getPriorityClass } from '@/utils/ui';
 import { getCurrentUser } from '@/lib/current-user';
 import { redirect } from 'next/navigation';
+import TicketItem from '@/components/TicketItem';
 
 const TicketsPage = async () => {
   const user = await getCurrentUser();
@@ -19,28 +18,7 @@ const TicketsPage = async () => {
       {tickets.length > 0 ? (
         <div className='space-y-4 max-w-3xl mx-auto'>
           {tickets.map((item) => (
-            <div
-              key={item.id}
-              className='flex justify-between items-center bg-white p-6 rounded-lg shadow border border-gray-200'
-            >
-              <h2 className='text-xl font-semibold text-blue-600'>
-                {item.subject}
-              </h2>
-              <div className='text-right space-y-2'>
-                <div className='text-sm text-gray-500'>
-                  Priority:{' '}
-                  <span className={getPriorityClass(item.priority)}>
-                    {item.priority}
-                  </span>
-                </div>
-                <Link
-                  href={`/tickets/${item.id}`}
-                  className='inline-block mt-2 bg-blue-600 text-white text-sm px-3 py-1 rounded hover:bg-blue-700 transition text-center'
-                >
-                  View Ticket
-                </Link>
-              </div>
-            </div>
+            <TicketItem key={item.id} ticket={item} />
           ))}
         </div>
       ) : (
