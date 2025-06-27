58fed8e37554ebd07d1dd3d5da054a5b7f6f05e0
SilvesterSpath <spath.sz@gmail.com>
2025-06-27 06:32:59 +0200

    CloseTicketButton closed

--- src/components/TicketItem.tsx
+++ src/components/TicketItem.tsx
@@ -7,10 +7,14 @@ type TicketItemProps = {
 };
 
 const TicketItem = ({ ticket }: TicketItemProps) => {
+  const isClosed = ticket.status === 'Closed';
+
   return (
     <div
       key={ticket.id}
-      className='flex justify-between items-center bg-white p-6 rounded-lg shadow border border-gray-200'
+      className={`flex justify-between items-center bg-white p-6 rounded-lg shadow border border-gray-200 ${
+        isClosed ? 'opacity-50' : ''
+      }`}
     >
       <h2 className='text-xl font-semibold text-blue-600'>{ticket.subject}</h2>
       <div className='text-right space-y-2'>
@@ -22,7 +26,11 @@ const TicketItem = ({ ticket }: TicketItemProps) => {
         </div>
         <Link
           href={`/tickets/${ticket.id}`}
-          className='inline-block mt-2 bg-blue-600 text-white text-sm px-3 py-1 rounded hover:bg-blue-700 transition text-center'
+          className={`inline-block mt-2  text-sm px-3 py-1 rounded  transition text-center ${
+            isClosed
+              ? 'bg-gray-500 text-gray-700 cursor-not-allowed pointer-events-none'
+              : 'bg-blue-600 text-white hover:bg-blue-700'
+          }`}
         >
           View Ticket
         </Link>
